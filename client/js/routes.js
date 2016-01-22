a3b3c1428fd55825ba32580b8609f712ec1ac780
Jacky <jaackyvan@gmail.com>
2016-01-21 19:14:13 -0500

    Modularizing skills & technologies into it's own template and file

--- client/js/routes.js
+++ client/js/routes.js
@@ -5,6 +5,7 @@ Router.route('/', function () {
  	this.render('sections', {to: 'sections'});
  	this.render('projects', {to: 'projects'});
  	this.render('experience', {to: 'experience'});
+ 	this.render('skills-and-technologies', {to: 'skills-and-technologies'});
 });
 
 Router.route('/bugsplat', function() {
