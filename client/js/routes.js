b36c0b38b9be42dc7f852decc415c6eac4913af7
Jacky <jaackyvan@gmail.com>
2016-01-21 19:11:27 -0500

    Modularizing experience  into it's own template and file

--- client/js/routes.js
+++ client/js/routes.js
@@ -4,6 +4,7 @@ Router.route('/', function () {
  	this.render('header');
  	this.render('sections', {to: 'sections'});
  	this.render('projects', {to: 'projects'});
+ 	this.render('experience', {to: 'experience'});
 });
 
 Router.route('/bugsplat', function() {
