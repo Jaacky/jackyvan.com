857fcf36eaca19f1d940c658a233982d174d1e83
Jacky <jaackyvan@gmail.com>
2016-01-21 19:09:23 -0500

    Modularizing projects into it's own template and file

--- client/js/routes.js
+++ client/js/routes.js
@@ -3,6 +3,7 @@ Router.configure({layoutTemplate: 'layout'});
 Router.route('/', function () {
  	this.render('header');
  	this.render('sections', {to: 'sections'});
+ 	this.render('projects', {to: 'projects'});
 });
 
 Router.route('/bugsplat', function() {
