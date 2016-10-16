335d7b11925f270c1fbe9daae7bea6c4dab657bb
Jacky <jaackyvan@gmail.com>
2016-10-16 00:20:04 -0400

    Nav menu now updates when scrolling

--- www/static/js/script.js
+++ www/static/js/script.js
@@ -1,16 +1,33 @@
 $(document).ready(function() {
-  $(".nav-menu").on("click", function() {
-    var id = "#" + $(this).html().trim().toLowerCase();
-    $('html, body').animate({
-        scrollTop: $(id).offset().top
-    }, 500);
-  });
-});
-
-$.fn.scrollView = function () {
-    return this.each(function () {
+    /*
+        Nav menu scrolling
+    */
+    $(".nav-menu").on("click", function() {
+        var id = "#" + $(this).html().trim().toLowerCase();
+        $(".nav-menu").removeClass("active");
+        $(this).addClass("active");
+        console.log($(this));
         $('html, body').animate({
-            scrollTop: $(this).offset().top
-        }, 1000);
+            scrollTop: $(id).offset().top
+        }, 500);
     });
-}
+
+    /*
+        Updating nav menu when scrolling
+        https://jsfiddle.net/cse_tushar/Dxtyu/141/
+    */
+    $(document).on("scroll", function() {
+        var scrollPos = $(document).scrollTop();
+        $('.nav-menu').each(function () {
+            var currLink = $(this);
+            var refElement = $("#" + currLink.html().trim().toLowerCase());
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('.nav-menu').removeClass("active");
+                currLink.addClass("active");
+            }
+            else{
+                currLink.removeClass("active");
+            }
+        });
+    });
+});
