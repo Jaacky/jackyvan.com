d4d8f2852a3570d24be173e0bc363ae9accc7865
Jacky <jaackyvan@gmail.com>
2016-10-17 19:53:02 -0400

    Tweaking nav scrolling highlighting so that it changes earlier

--- www/static/js/script.js
+++ www/static/js/script.js
@@ -17,11 +17,24 @@ $(document).ready(function() {
         https://jsfiddle.net/cse_tushar/Dxtyu/141/
     */
     $(document).on("scroll", function() {
-        var scrollPos = $(document).scrollTop();
+        var scrollPos = $(document).scrollTop() + 200;
         $('.nav-navigate-within').each(function () {
             var currLink = $(this);
             var refElement = $("#" + currLink.html().trim().toLowerCase());
-            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+
+            /*
+                Useful debugging statements for tweaking scrolling -Jacky
+            */
+            // console.log(
+            //     "Ref top: " + refElement.position().top + 
+            //     ", ref top + height: " + String(refElement.position().top + refElement.outerHeight()) + 
+            //     ", scroll pos: " + scrollPos, $(this));
+            // console.log(
+            //     "If condition: " + String(refElement.position().top <= scrollPos && refElement.position().top + refElement.outerHeight() >= scrollPos) +
+            //     ", first part of if: " + String(refElement.position().top <= scrollPos) + 
+            //     ", second part of it: " + String(refElement.position().top + refElement.outerHeight() >= scrollPos));
+
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.outerHeight() >= scrollPos) {
                 $('.nav-menu').removeClass("active");
                 currLink.addClass("active");
             }
