4d34a8bbd8c1d3ce99d8d50c1dc486e184b98a32
Jacky <jaackyvan@gmail.com>
2016-11-14 20:33:32 -0500

    Adding bug splat game and a route to it

--- www/views.py
+++ www/views.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route('/')
 def home(name=None):
     return render_template('index.html')
+
+@app.route('/bugSplat')
+def bugSplat(name=None):
+    return render_template('bugSplat.html', bugSplat='true')
